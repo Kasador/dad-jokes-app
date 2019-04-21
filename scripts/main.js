3522e6326a56e98efe4efb0ffbe00637a8762a6d
Kasador <huntershaw23@gmail.com>
2019-04-21 11:45:24 -0600

    fixed appended feature to not append again and again if already appended.

--- scripts/main.js
+++ scripts/main.js
@@ -2,73 +2,73 @@ $(document).ready(function(){
 //=========================== F U N C T I O N S ====================================//
 //=========================== F U N C T I O N S ====================================//
 //=========================== F U N C T I O N S ====================================//
-    // function to get random card
-        function generateCard(card) {
-        // get cards and the count
-            let cards = $('.card');
-            let cardsLen = cards.length;
+// function to get random card
+    function generateCard(card) {
+    // get cards and the count
+        let cards = $('.card');
+        let cardsLen = cards.length;
 
-        // generate random number based on amount of cards
-            let indexNum = Math.floor(Math.random() * cardsLen);
+    // generate random number based on amount of cards
+        let indexNum = Math.floor(Math.random() * cardsLen);
 
-        // get the card that matches array value of random num
-            let getCard = cards[indexNum];
+    // get the card that matches array value of random num
+        let getCard = cards[indexNum];
 
-        // set id to the card number
-            let idNum = (indexNum + 1);
-            getCard.id = idNum;
+    // set id to the card number
+        let idNum = (indexNum + 1);
+        getCard.id = idNum;
 
-        // return card info
-            return getCard;
-        }
+    // return card info
+        return getCard;
+    }
 
-    // remove last card function
-        function removeLastCard(current) {
-            // get current card
-            let currentCard = $(".card:visible");
-            return currentCard;
-        }
+// remove last card function
+    function removeLastCard(current) {
+        // get current card
+        let currentCard = $(".card:visible");
+        return currentCard;
+    }
 
-         // save data
-         function saveData(data) {
-            localStorage.setItem(data, unsave);
-        }
+        // save data
+        function saveData(data) {
+        localStorage.setItem(data, unsave);
+    }
 
-        // delete data
-        function deleteData(data) {
-            localStorage.removeItem(data);
-        }
-        
-        // save card title
-        function saveTitle(data) {
-            let savedTitle = getCurrentTitle();
-            localStorage.setItem(data, savedTitle);
-        }
-        // save card text
-        function saveText(data) {
-            let savedText = getCurrentText();
-            localStorage.setItem(data, savedText);
-        }
+    // delete data
+    function deleteData(data) {
+        localStorage.removeItem(data);
+    }
+    
+    // save card title
+    function saveTitle(data) {
+        let savedTitle = getCurrentTitle();
+        localStorage.setItem(data, savedTitle);
+    }
+    // save card text
+    function saveText(data) {
+        let savedText = getCurrentText();
+        localStorage.setItem(data, savedText);
+    }
 
-        // delete card title
-        function deleteTitle(data) {
-            localStorage.removeItem(data);
-        }
+    // delete card title
+    function deleteTitle(data) {
+        localStorage.removeItem(data);
+    }
 
-        // delete card text
-        function deleteText(data) {
-            localStorage.removeItem(data);
-        }
+    // delete card text
+    function deleteText(data) {
+        localStorage.removeItem(data);
+    }
 
-        function getCurrentTitle() {
-            let savedTitle = $(".card:visible").find('.card-title').text();
-            return savedTitle;
-        }
+    function getCurrentTitle() {
+        let savedTitle = $(".card:visible").find('.card-title').text();
+        return savedTitle;
+    }
 
-        function getCurrentText() {
-            let savedText = $(".card:visible").find('.card-text').text();
-            return savedText;
-        }
+    function getCurrentText() {
+        let savedText = $(".card:visible").find('.card-text').text();
+        return savedText;
+    }
 
 //=========================== end of F U N C T I O N S ====================================//
 //=========================== end of F U N C T I O N S ====================================//
@@ -77,137 +77,128 @@ $(document).ready(function(){
     // display first card
     let firstLoaded = true;
 
-    // display first card only on load
-        do {
-            generateCard().style.display = 'block';
-            firstLoaded = false;
+// display first card only on load
+    do {
+        generateCard().style.display = 'block';
+        firstLoaded = false;
+    }
+    while (firstLoaded == true);
+
+    // variables for save button
+    let save = 'Save <i class="fa fa-star" aria-hidden="true"></i>';
+    let unsave = 'Unsave <i class="fa fa-trash" aria-hidden="true"></i>';
+    let cardId = $(".card:visible").attr("id");
+
+    // get another random card on random btn click
+    $('.ranBtn').on('click', function() {
+        // remove last card and get new
+        removeLastCard().hide();
+        generateCard().style.display = 'block';
+
+        // get id of current card
+        let saveId = $(".card:visible").attr("id");
+
+        // if saved change save btn
+        if (localStorage.getItem('button ' + saveId) === null) {
+            $('.saveBtn').html(save);
+        } else if (localStorage.getItem('button ' + saveId) !== null) {
+            $('.saveBtn').html(localStorage.getItem('button ' + saveId));
         }
-        while (firstLoaded == true);
+    });
+    // change save btn text to unsave on click, (toggle)
+    $('.saveBtn').on('click', function() {
+        // get current card plus id
+        let getCurrentCard = $(".card:visible");
+        let saveId = getCurrentCard.attr("id");
 
-        // variables for save button
-        let save = 'Save <i class="fa fa-star" aria-hidden="true"></i>';
-        let unsave = 'Unsave <i class="fa fa-trash" aria-hidden="true"></i>';
-        let cardId = $(".card:visible").attr("id");
+        // save card and put into localStorage (toggle)
+        if ($('.saveBtn').html() == save) {
+            // save card
+            console.log('Card ' + saveId + ' has been saved!');
+            saveData('button ' + saveId);
 
-        // get another random card on random btn click
-        $('.ranBtn').on('click', function() {
-            // remove last card and get new
-            removeLastCard().hide();
-            generateCard().style.display = 'block';
-
-            // get id of current card
-            let saveId = $(".card:visible").attr("id");
-
-            // if saved change save btn
-            if (localStorage.getItem('button ' + saveId) === null) {
-                $('.saveBtn').html(save);
-            } else if (localStorage.getItem('button ' + saveId) !== null) {
-                $('.saveBtn').html(localStorage.getItem('button ' + saveId));
+            // id the card to get card info
+            getCurrentCard.addClass(saveId);
+            if (getCurrentCard.hasClass(saveId)) {
+                saveTitle('title ' + saveId);
+                saveText('text ' + saveId);
             }
-        });
+        } else if (localStorage.getItem('button ' + saveId) !== null) {
+            // delete card
+            console.log('Card ' + saveId + ' has been unsaved!');
+            deleteData('button ' + saveId);
+            getCurrentCard.removeClass(saveId);
 
-        // change save btn text to unsave on click, (toggle)
-        $('.saveBtn').on('click', function() {
-            // get current card plus id
-            let getCurrentCard = $(".card:visible");
-            let saveId = getCurrentCard.attr("id");
-
-            // save card and put into localStorage (toggle)
-            if ($('.saveBtn').html() == save) {
-                // save card
-                console.log('Card ' + saveId + ' has been saved!');
-                saveData('button ' + saveId);
-
-                // id the card to get card info
-                getCurrentCard.addClass(saveId);
-                if (getCurrentCard.hasClass(saveId)) {
-                    saveTitle('title ' + saveId);
-                    saveText('text ' + saveId);
-                }
-            } else if (localStorage.getItem('button ' + saveId) !== null) {
-                // delete card
-                console.log('Card ' + saveId + ' has been unsaved!');
-                deleteData('button ' + saveId);
-                getCurrentCard.removeClass(saveId);
-
-                // remove the id of card to delete card info
-                if (!getCurrentCard.hasClass(saveId)) {
-                    deleteTitle('title ' + saveId);
-                    deleteText('text ' + saveId);
-                }
+            // remove the id of card to delete card info
+            if (!getCurrentCard.hasClass(saveId)) {
+                deleteTitle('title ' + saveId);
+                deleteText('text ' + saveId);
             }
-
-            // change save btn to unsave if item is saved
-            if (localStorage.getItem('button ' + saveId) === null) {
-                $('.saveBtn').html(save);
-            } else {
-                $('.saveBtn').html(unsave);
-            }
-        });
-
-        // on load change save btn to unsave if item in localStorage
-        if (localStorage.getItem('button ' + cardId) === null) {
+        }
+        // change save btn to unsave if item is saved
+        if (localStorage.getItem('button ' + saveId) === null) {
             $('.saveBtn').html(save);
         } else {
-            $('.saveBtn').html(localStorage.getItem('button ' + cardId));
+            $('.saveBtn').html(unsave);
         }
+    });
+    // on load change save btn to unsave if item in localStorage
+    if (localStorage.getItem('button ' + cardId) === null) {
+        $('.saveBtn').html(save);
+    } else {
+        $('.saveBtn').html(localStorage.getItem('button ' + cardId));
+    }
+    // toggle pages on button clicks of menu
+    let favoritesPage = $('.favorites-page'),
+        settingsPage = $('.settings-page');
 
-        // toggle pages on button clicks of menu
-        let favoritesPage = $('.favorites-page'),
-            settingsPage = $('.settings-page');
+    // favorites page
+    $('.favorites').on('click', function(){
+        favoritesPage.animate({width:'toggle'},350);
 
-        // favorites page
-        $('.favorites').on('click', function(){
-            favoritesPage.animate({width:'toggle'},350);
-
-            if (settingsPage.css('display') == 'block') {
-                settingsPage.animate({width:'toggle'},350);
-            }
-        });
-        
-        // settings page
-        $('.settings').on('click', function(){
+        if (settingsPage.css('display') == 'block') {
             settingsPage.animate({width:'toggle'},350);
+        }
+    });
+    // settings page
+    $('.settings').on('click', function(){
+        settingsPage.animate({width:'toggle'},350);
 
-            if (favoritesPage.css('display') == 'block') {
-                favoritesPage.animate({width:'toggle'},350);
-            }
-        });
-
-        // home page
-        $('.home').on('click', function(){
-            if (favoritesPage.css('display') == 'block') {
-                favoritesPage.animate({width:'toggle'},350);
-            } else if (settingsPage.css('display') == 'block') {
-                settingsPage.animate({width:'toggle'},350);
-            }
-        });
+        if (favoritesPage.css('display') == 'block') {
+            favoritesPage.animate({width:'toggle'},350);
+        }
+    });
+    // home page
+    $('.home').on('click', function(){
+        if (favoritesPage.css('display') == 'block') {
+            favoritesPage.animate({width:'toggle'},350);
+        } else if (settingsPage.css('display') == 'block') {
+            settingsPage.animate({width:'toggle'},350);
+        }
+    });
+        // check and setInterval
+    setInterval( () => {
+        console.log('looping');
+        // loop through all cards and append if saved
         for (let i=1; i < $('.card').length + 1; i++) {
-            // console.log('Number ' + i);
+            // variables for localStorage items
             let title = localStorage.getItem('title ' + i),
                 text = localStorage.getItem('text ' + i),
-                btn = localStorage.getItem('button ' + i);
-
+                btn = localStorage.getItem('button ' + i),
+                checkCard = $('#card-append'+i);
+            // check if there is data in localStorage, if so, append
             if (title !== null && text !== null && btn !== null) {
-                // append cards function
-                $('.append-cards').append(
-                    '<div class="card-body border"><h5 class="card-title title-append'+i+'"></h5><p class="card-text text-append'+i+'"></p><a href="#" class="btn btn-primary saveBtn notSaved btn-append'+i+'"></a></div>'
-                );
-                $('.title-append' + i).append(title);
-                $('.text-append' + i).append(text);
-                $('.btn-append' + i).append(btn);
+                // check if already appended, so if, don't append again
+                if (checkCard.length == 0) {
+                    // append cards
+                    $('.append-cards').append(
+                        '<div class="card-body border" id="card-append'+i+'"><h5 class="card-title title-append'+i+'"></h5><p class="card-text text-append'+i+'"></p><a href="#" class="btn btn-primary saveBtn notSaved btn-append'+i+'"></a></div>'
+                    );
+                    $('.title-append' + i).append(title);
+                    $('.text-append' + i).append(text);
+                    $('.btn-append' + i).append(btn);
+                }
             }
         }
-        // check if there is anything in local storage
-        // setInterval( () => {
-        //     if (localStorage.getItem("button 2") !== null) {
-        //         // append cards function
-        //         $('.append-cards').append(
-        //             '<div class="card-body border"><h5 class="card-title title-append"></h5><p class="card-text text-append">some</p><a href="#" class="btn btn-primary saveBtn notSaved btn-append"></a></div>'
-        //         );
-        //       }
-        // },1000);
-        // let test = localStorage.getItem('button 2');
-        // $('.append-cards').append(test);
-
+    }, 1000);
 });
